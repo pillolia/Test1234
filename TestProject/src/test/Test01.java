b3ce9f18c82dd4bd773c8a997726089a42553b76
onelovehj <onelovehj@hotmail.com>
2021-04-23 16:46:44 +0900

    야 나도에서 수정함

--- TestProject/src/test/Test01.java
+++ TestProject/src/test/Test01.java
@@ -4,8 +4,8 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi !!! :D");
-		
+		System.out.println("hello !!! :D");
+		/////////////////////////
 	}
 
 }
